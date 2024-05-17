bd7bc330bc79182c35376c220c0d784742dac8c1
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-17 12:22:25 -0300

    chore: handle edgecases for 'nextjs', 'reactjs', 'tailwind' & 'chakraui'

--- src/utils/filter.py
+++ src/utils/filter.py
@@ -4,10 +4,29 @@ def filter_text():
     print('Enter keywords one at a time')
 
     while True:
-        user_input = input("Next word: ")
+        user_input = input("Next word: ").lower()
         
         if user_input == 'exit':
             break
+        elif user_input == 'nextjs':
+            keywords.append(user_input)
+            keywords.append('next.js')
+            keywords.append('next js')
+            print("Keywords: ", keywords)
+        elif user_input == 'react':
+            keywords.append(user_input)
+            keywords.append('react.js')
+            keywords.append('react js')
+            print("Keywords: ", keywords)
+        elif user_input == 'tailwind':
+            keywords.append(user_input)
+            keywords.append('tailwindcss')
+            print("Keywords: ", keywords)
+        elif user_input == 'chakra ui':
+            keywords.append(user_input)
+            keywords.append('chakraui')
+            keywords.append('chakra')
+            print("Keywords: ", keywords)
         else:
             keywords.append(user_input)
             print("Keywords: ", keywords)
