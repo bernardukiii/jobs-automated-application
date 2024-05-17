e0e9e89f24b3d7235396831ba265eea69071bce2
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-17 12:13:48 -0300

    chore: ask user for  keywords

--- src/app.py
+++ src/app.py
@@ -1,5 +1,14 @@
 from utils.filter import filter_text
 
-print('Hello world!')
+print("Welcome to Jobs Automated")
+print("We provide jobs descriptions filtering to see if it matches what you're looking for and we take care of automating the application form with repetitive information.")
+print("Navigation commands:")
+print("'done' => breaks out of loop")
 
-filter_text()
+requested_tool = input("Please select either filtering (DF) or automation (FA): ")
+
+if requested_tool == 'DF':
+    print("Let's filter that description!")
+    filter_text()
+elif requested_tool == 'FA':
+    print("Let's automate that form!")
--- src/utils/filter.py
+++ src/utils/filter.py
@@ -1,2 +1,14 @@
 def filter_text():
-    print('Hello world from filter function!!')
+    keywords = []
+
+    print('Enter keywords one at a time')
+
+    while True:
+        user_input = input("Next word: ")
+        
+        if user_input == 'exit':
+            break
+        else:
+            keywords.append(user_input)
+            print("Keywords: ", keywords)
+    # job_description = input("Please enter the job description (copy/paste allowed): ")
