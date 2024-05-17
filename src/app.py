4ef43bf368f7fbcead4b6e12a81a4319ead757e4
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-17 17:44:47 -0300

    chore: prompting user for  data

--- src/app.py
+++ src/app.py
@@ -1,6 +1,18 @@
 from utils.filter import filter_text
 from utils.prefill_form import prefill_form
 
+user_info = {
+  "name": "",
+  "surname": "",
+  "email": "",
+  "number": "",
+  "linkedin": "",
+  "github": "",
+  "portfolio": "",
+  "city": "",
+  "formURL": ""
+}
+
 print("Welcome to Jobs Automated")
 print("We provide jobs descriptions filtering to see if it matches what you're looking for and we take care of automating the application form with repetitive information.")
 print("Navigation commands:")
@@ -13,5 +25,11 @@ if requested_tool == 'df':
     filter_text()
 elif requested_tool == 'fa':
     print("Let's automate that form!")
-    prefill_form('https://lapaginamillonaria.com/')
+    print("Let's start by providing the information for me to prefill the form with: ")
+    
+    for field in user_info:
+        field = input(f"Please enter your {field}:")
+    print(user_info)
+
+    # prefill_form('https://lapaginamillonaria.com/')
     
--- src/utils/prefill_form.py
+++ src/utils/prefill_form.py
@@ -2,7 +2,7 @@ import re
 from playwright.sync_api import sync_playwright, Page
 
 
-def prefill_form(url: str):    
+def prefill_form(user_info: dict):    
     with sync_playwright() as p:
         # Launch Chromium browser with headless set to False
         browser = p.chromium.launch(headless=False)
@@ -10,7 +10,7 @@ def prefill_form(url: str):
         page = context.new_page()
 
         # Now navigate to the URL
-        page.goto(url)
+        page.goto()
                 
         # Close the browser after the test
         browser.close()
