7a0bdec31b835d62ea83dceca1cac9bed7b0d62e
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-17 12:44:40 -0300

    chore: remove punctuation marks in description + lowercase all words

--- src/utils/filter.py
+++ src/utils/filter.py
@@ -1,4 +1,7 @@
+import string
+
 def filter_text():
+    # PROMPT USER FOR KEYWORDS USER WOULD LIKE TO USE TO FILTER
     keywords = []
 
     print('Enter keywords one at a time')
@@ -30,4 +33,27 @@ def filter_text():
         else:
             keywords.append(user_input)
             print("Keywords: ", keywords)
-    # job_description = input("Please enter the job description (copy/paste allowed): ")
+    
+    # PROMPT FOR THE DESCRIPTION USER WOULD LIKE TO FILTER THROUGH
+    job_description_lines = []
+    lowered_job_description = []
+
+    print("Please enter the job description (copy/paste allowed). Type 'end' on a new line to finish:")
+    # handle long descriptions as they have line spaces that cause the terminal to break out of app
+    while True:
+        line = input()
+        if line.strip().lower() == 'end':
+            break
+        job_description_lines.append(line)
+    
+    job_description = "\n".join(job_description_lines)
+    # Remove punctuation from the job description ## avoids edgecases like when the texts says 'react, tailwind and html' if we search for react, it will not find it due to the comma
+    job_description = job_description.translate(str.maketrans('', '', string.punctuation))
+        
+    job_description_list = job_description.split()
+    # Every word in job description to lowercase
+    for word in job_description_list:
+        lowercase_word = word.lower()
+        lowered_job_description.append(lowercase_word)
+
+    print('This should be a list of strings:', lowered_job_description)
