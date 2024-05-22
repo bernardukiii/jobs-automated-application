d742e55de254aea48bac5f78dbeb876568cd087d
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-22 10:52:08 -0300

    chore: first five samples of what a form could look like for automation

--- src/utils/prefill_form.py
+++ src/utils/prefill_form.py
@@ -12,6 +12,57 @@ def prefill_form(user_info: dict):
 
         # Now navigate to the URL
         page.goto(user_info["formURL"])
-                
-        # Close the browser after the test
-        browser.close()
+        
+        # Sample one
+        page.get_by_label("Name").click()
+        page.get_by_label("Name").fill("name")
+        page.get_by_placeholder("hello@example.com...").click()
+        page.get_by_placeholder("hello@example.com...").fill("email")
+        page.get_by_label("LinkedIn URL").click()
+        page.get_by_label("LinkedIn URL").fill("linkedinlink")
+        # Sample two
+        page.get_by_placeholder("Type here...").click()
+        page.get_by_placeholder("Type here...").fill("ber")
+        page.get_by_placeholder("hello@example.com...").click()
+        page.get_by_placeholder("hello@example.com...").fill("ber")
+        # Sample three
+        page.get_by_label("Full name✱").click()
+        page.get_by_label("Full name✱").fill("ber")
+        page.get_by_label("Email✱").click()
+        page.get_by_label("Email✱").fill("ber")
+        page.get_by_label("Phone").click()
+        page.get_by_label("Phone").fill("234")
+        page.get_by_label("LinkedIn (Keep in mind that a").click()
+        page.get_by_label("LinkedIn (Keep in mind that a").fill("linkedin")
+        page.get_by_label("GitHub (If you are not a").click()
+        page.get_by_label("GitHub (If you are not a").fill("github")
+        page.get_by_label("Portfolio (If you are not a").click()
+        page.get_by_label("Portfolio (If you are not a").fill("portfolio")
+        # Sample four
+        page.get_by_label("First Name*").click()
+        page.get_by_label("First Name*").fill("ber")
+        page.get_by_label("Last Name*").click()
+        page.get_by_label("Last Name*").fill("ferr")
+        page.get_by_label("Email*").click()
+        page.get_by_label("Email*").fill("ema")
+        page.get_by_role("textbox", name="City*").fill("b")
+        page.get_by_role("textbox", name="City*").click()
+        page.get_by_role("textbox", name="City*").fill("bsas")
+        # Sample five
+        page.locator("[id=\"applicant_lead_attributes\\[first_name\\]\"]").click()
+        page.locator("[id=\"applicant_lead_attributes\\[first_name\\]\"]").fill("ber")
+        page.locator("[id=\"applicant_lead_attributes\\[last_name\\]\"]").click()
+        page.locator("[id=\"applicant_lead_attributes\\[last_name\\]\"]").fill("ferr")
+        page.locator("[id=\"applicant_lead_attributes\\[email\\]\"]").click()
+        page.locator("[id=\"applicant_lead_attributes\\[email\\]\"]").fill("ema")
+        page.locator("[id=\"applicant_lead_attributes\\[phone\\]\"]").click()
+        page.locator("[id=\"applicant_lead_attributes\\[phone\\]\"]").fill("123")
+        page.get_by_placeholder("Hyperlink").click()
+        page.get_by_placeholder("Hyperlink").fill("links")
+        # Sample six
+        
+
+
+
+        # # Close the browser after the test
+        # browser.close()
