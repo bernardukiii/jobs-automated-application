1c0db74b1f6365b2950a22b1edb01713857c1d5b
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-22 15:49:42 -0300

    chore: same for surname

--- src/utils/prefill_form.py
+++ src/utils/prefill_form.py
@@ -13,8 +13,10 @@ def prefill_form(user_info: dict):
         # Now navigate to the URL
         page.goto(user_info["formURL"])
         
+
+# As different forms have different ways of handling elements, this will try a few options but might not coincide and fail
+
         # Try filling in the name
-        # As different forms have different ways of handling elements, this will try a few options but might not coincide and fail
         try:
             page.get_by_label("Name").click()
             page.get_by_label("Name").fill(user_info["name"])
@@ -37,7 +39,28 @@ def prefill_form(user_info: dict):
         except TimeoutError:
             print("Could not fill in 'NAME'")
         
+        # Try filling in the surname
+        try:
+            page.get_by_label("Last Name*").click()
+            page.get_by_label("Last Name*").fill(user_info["surname"])
 
+            if TimeoutError:
+                page.locator("[id=\"applicant_lead_attributes\\[last_name\\]\"]").click()
+                page.locator("[id=\"applicant_lead_attributes\\[last_name\\]\"]").fill(user_info["surname"])
+            elif TimeoutError:
+                page.get_by_label("Last Name *").click()
+                page.get_by_label("Last Name *").fill(user_info["surname"])
+            elif TimeoutError:
+                page.get_by_placeholder("Apellidos").click()
+                page.get_by_placeholder("Apellidos").fill(user_info["surname"])
+            elif TimeoutError:
+                page.get_by_placeholder("Last Name").click()
+                page.get_by_placeholder("Last Name").fill(user_info["surname"])
+        except TimeoutError:
+            print("Could not fill in 'SURNAME'")
+
+    
+ 
 
         # # Close the browser after the test
         # browser.close()
