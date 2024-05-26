3208239c435facdfd2c37a280d7fb07c5903cd01
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-26 14:41:10 -0300

    feat: add class with functions to save data

---
+++ src/utils/handle_data.py
@@ -0,0 +1,32 @@
+import pickle, os
+
+class HandleData:
+    # Initializer #
+    def __init__(self, file_name):
+        self.file_name = file_name
+        
+# First function to check if peexisting information exists
+    def check_data(self, user_name):
+        filename = user_name.lower()
+        file_path = os.path.join(self.data_dir, f'{filename}.pickle')
+
+        if os.path.exists(file_path):
+            with open(f'/data/{filename}.pickle', 'rb') as file:
+                existing_data = pickle.load(file)
+                print('This is your saved information:')
+                for field in existing_data:
+                    print(field[field])
+        else:
+            print('No preexisting information.')
+
+# Save information
+    def save_data(self, data):
+        # Check if the data in fact exists
+        if data:
+            filename = data["name"].lower()
+            file_path = os.path.join(self.data_dir, f'{filename}.pickle')
+
+            with open(file_path, 'wb') as file:
+                pickle.dump(data, file)
+        else:
+            print('No information provided.')
