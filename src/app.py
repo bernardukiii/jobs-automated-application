0b3a6d4c8615bc653129252dde4be777234391a0
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-26 16:08:04 -0300

    chore: calling check data in app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 from utils.filter import filter_text
 from utils.prefill_form import prefill_form
-
+from utils.handle_data import HandleData
 
 fields = ["name", "surname", "email", "number", "linkedin", "github", "portfolio", "city", "formURL"] 
 user_info = {}
+data_handler = HandleData()
 
 print("Welcome to Jobs Automated")
 print("We provide jobs descriptions filtering to see if it matches what you're looking for and we take care of automating the application form with repetitive information.")
@@ -19,11 +20,19 @@ elif requested_tool == 'fa':
     print("Let's automate that form!")
     print("Let's start by providing the information for me to prefill the form with: ")
     
-    for field in fields:
-        user_info[field] = input(f"Please enter your {field}: ")
-        if field == '':
-            print('Sorry, this field is required...')
-            field = input(f"Please enter your {field}: ")
+    first_time = input('Is this your first time using the app? (y/n) ').lower()
+
+    if first_time == "y":
+        for field in fields:
+            user_info[field] = input(f"Please enter your {field}: ")
+            if field == '':
+                print('Sorry, this field is required...')
+                field = input(f"Please enter your {field}: ")
+    elif first_time == "n":
+        search_by_name = input("Please enter your name: ").lower()
+        if search_by_name != '':
+            data_handler.check_data(search_by_name)
+
 
     prefill_form(user_info)
     
